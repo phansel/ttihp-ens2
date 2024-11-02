9d62b12ad3fead0347799d8d91b0f4de3ffe5438
phansel <github@paulhansel.com>
2024-11-02 12:06:40 -0700

    actually increase to 47

--- src/tex/pack-verilog.py
+++ src/tex/pack-verilog.py
@@ -3,7 +3,7 @@
 tex = open("transforms.tex", 'r')
 ver = open("../transforms.v", 'w')
 print("trying to open transforms.tex")
-linestoread=46
+linestoread=47
 line = 0
 
 startmemline = """module memory_chars(
