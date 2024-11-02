b2737c001c43611ce425e9285976be62682ff6a4
phansel <github@paulhansel.com>
2024-11-02 12:12:51 -0700

    try 45

--- src/tex/pack-verilog.py
+++ src/tex/pack-verilog.py
@@ -3,7 +3,7 @@
 tex = open("transforms.tex", 'r')
 ver = open("../transforms.v", 'w')
 print("trying to open transforms.tex")
-linestoread=46
+linestoread=45
 line = 0
 
 startmemline = """module memory_chars(
